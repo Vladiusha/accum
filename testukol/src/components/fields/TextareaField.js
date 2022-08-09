b33793ad9e33d46b7ca3bb2f3e45151b8e4ada34
Vladiusha <kalachikov.moyo@gmail.com>
2022-08-09 20:47:37 +0200

    Add custom color

--- testukol/src/components/fields/TextareaField.js
+++ testukol/src/components/fields/TextareaField.js
@@ -6,7 +6,7 @@ const TextareaField = (props) => {
     const [value, setValue] = useState(props.value)
 
     return (<div className={classNames(styles.field, styles.textareaHeight)}>
-        <div className={styles.label}>{props.label}</div>
+        <label className={styles.label}>{props.label}</label>
         <div className={styles.textareaValue}>
                 <textarea className={styles.textareaValueField}
                           value={value}
